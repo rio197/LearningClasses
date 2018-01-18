375166d698955d1de7a1f28f4f663bbb8bc4b71b
Thomas <thomas@test.com>
2018-01-18 12:38:11 -0500

    Initial commit

---
+++ src/learningclasses/LearningClasses.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learningclasses;
+
+/**
+ *
+ * @author tmuchnedy
+ */
+public class LearningClasses {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vehicles car = new Vehicles;
+        car.vehicleName = "BMW 1 Series";
+    }
+    
+    static class Vehicles {
+        String vehicleName;
+        int numberOfTires;
+        double topSpeed;
+    }    
+}
